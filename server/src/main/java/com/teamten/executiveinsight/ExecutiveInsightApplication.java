6d97b8b2d85997cf1ceb8ba650a2331a3acbbea4
nihalislam01 <nihalislam2@gmail.com>
2024-02-14 21:22:55 +0600

    Add Spring Boot

---
+++ server/src/main/java/com/teamten/executiveinsight/ExecutiveInsightApplication.java
@@ -0,0 +1,13 @@
+package com.teamten.executiveinsight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExecutiveInsightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExecutiveInsightApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/teamten/executiveinsight/ExecutiveInsightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamten.executiveinsight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExecutiveInsightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
