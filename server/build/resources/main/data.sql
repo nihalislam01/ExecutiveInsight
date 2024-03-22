69052796280f13ac206239acd55fafe4a30eae5f
nihalislam01 <nihalislam2@gmail.com>
2024-03-22 23:37:19 +0600

    Draft

--- server/build/resources/main/data.sql
+++ server/build/resources/main/data.sql
@@ -3,7 +3,8 @@ insert into workspace(workspace_id, name, code, business_title_id) values(2001, 
 
 insert into users(user_id, email, name, password, role, bio, is_enable, workspace_id) values(1001, 'nihalislam2@gmail.com', 'Nihal Islam', '$2a$10$T16zWLV3ZfFKiwMw8y/9POiRhIXPCOhtujhCegTeXkg2rBGyM5H9O', 'ADMIN', 'Im a web dev student', true, 2001),
 (1002, 'shsakin2002@gmail.com', 'Sakin', '$2a$10$T16zWLV3ZfFKiwMw8y/9POiRhIXPCOhtujhCegTeXkg2rBGyM5H9O', 'ADMIN', 'Im a DBA', true, 2002),
-(1003, 'nihal.islam@g.bracu.ac.bd', 'Nihal', '$2a$10$T16zWLV3ZfFKiwMw8y/9POiRhIXPCOhtujhCegTeXkg2rBGyM5H9O', 'USER', 'Im a Student', true, null);
+(1003, 'nihal.islam@g.bracu.ac.bd', 'Nihal', '$2a$10$T16zWLV3ZfFKiwMw8y/9POiRhIXPCOhtujhCegTeXkg2rBGyM5H9O', 'USER', 'Im a Student', true, null),
+(1004, 'shahrin.sultana@g.bracu.ac.bd', 'Shahrin Sultana', '$2a$10$T16zWLV3ZfFKiwMw8y/9POiRhIXPCOhtujhCegTeXkg2rBGyM5H9O', 'USER', 'Im a Student', true, null);
 
 update workspace set user_id = 1001 where workspace_id = 2001;
 update workspace set user_id = 1002 where workspace_id = 2002;
